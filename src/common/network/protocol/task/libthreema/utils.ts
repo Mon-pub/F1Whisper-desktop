e8b57584b4b6c2e7d003654baa56860427bf78b5
André Gaillard <andre.gaillard@threema.ch>
2025-08-21 15:53:04 +0200

    Add work context helper for libthreema

--- src/common/network/protocol/task/libthreema/utils.ts
+++ src/common/network/protocol/task/libthreema/utils.ts
@@ -1,8 +1,13 @@
-import type {ClientInfo} from 'libthreema';
+import type {ClientInfo, WorkContext} from 'libthreema';
 
 import type {ServicesForBackend} from '~/common/backend';
+import type {ThreemaWorkData} from '~/common/device';
 import {getBrowserInfo} from '~/common/dom/utils/browser';
+import {assert} from '~/common/utils/assert';
 
+/**
+ * Create a {@link ClientInfo} object used by libthreema.
+ */
 export function getClientInfo(services: Pick<ServicesForBackend, 'systemInfo'>): ClientInfo {
     const {arch, os, locale} = services.systemInfo;
     const {name, version} = getBrowserInfo(self.navigator.userAgent);
@@ -15,4 +20,25 @@ export function getClientInfo(services: Pick<ServicesForBackend, 'systemInfo'>):
         osName: os,
         osArchitecture: arch,
     };
+}
+
+/**
+ * Create a {@link WorkContext} object used by libthreema.
+ *
+ * @throws If called in a consumer build.
+ */
+export function createWorkContext(workData: ThreemaWorkData): WorkContext {
+    assert(
+        import.meta.env.BUILD_VARIANT !== 'consumer',
+        'Work context cannot be created in consumer build',
+    );
+    if (import.meta.env.BUILD_ENVIRONMENT === 'onprem') {
+        return {credentials: {...workData.workCredentials}, flavor: 'on-prem'};
+    }
+
+    if (import.meta.env.BUILD_VARIANT === 'custom') {
+        return {credentials: {...workData.workCredentials}, flavor: 'on-prem'};
+    }
+
+    return {credentials: {...workData.workCredentials}, flavor: 'work'};
 }
